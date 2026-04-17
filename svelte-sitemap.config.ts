ec6de8279182e6d044ff5517ab68c6d3dfa5eb59
dan3988 <40858622+dan3988@users.noreply.github.com>
2026-04-17 21:09:55 +0100

    Added sitemap generator

---
+++ svelte-sitemap.config.ts
@@ -0,0 +1,8 @@
+import type { OptionsSvelteSitemap } from 'svelte-sitemap';
+
+const config: OptionsSvelteSitemap = {
+	domain: 'https://www.dpickett.co.uk',
+	resetTime: true,
+};
+
+export default config;
